c51dc168aed3a280014f1ca153a49e8b8fbf9170
dylan2013 <dylan.huang@ischool.com.tw>
2013-08-08 09:58:02 +0800

    新增練習檔案

---
+++ dylan/Sample02取得課程資料/js/test.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    var conn = gadget.getContract("basic.teacher");
+    conn.send({
+        service: "GetMyCourse",
+        body: "",
+        result: function (response, error, http) {
+            if (error === null) {
+                alert("讀取成功!!");
+                var courseString = '';
+                $(response.Course).each(function (index, item) {
+                    courseString += "<tr>";
+                    courseString += "<td>" + item.CourseId + "</td>";
+                    courseString += "<td>" + item.CourseName + "</td>";
+                    courseString += "<td>" + item.Subject + "</td>";
+                    courseString += "<td>" + item.SchoolYear + "</td>";
+                    courseString += "<td>" + item.Semester + "</td>";
+                    courseString += "<td>" + item.Sequence + "</td>";
+                    courseString += '</tr>';
+                });
+                $('#tbodyIsTrue').html(courseString);
+            }
+            else {
+                alert(error);
+            };
+        }
+    })
+});
