5e9e84a8815082f235c3703e9382e224580efd44
huangwcchhs <huangwc@chhs.hcc.edu.tw>
2013-09-02 15:07:51 +0800

    first gadget

---
+++ huangwc/first_gadget/js/default.js
@@ -0,0 +1,23 @@
+﻿function CourseCtrl($scope)
+{
+	$scope.courses = [];
+	$scope.init = function(){
+		var connection = gadget.getContract("basic.teacher");
+		connection.send({
+			service: "beta.GetMyCourse",
+			body: '<Request><Condition></Condition></Request>',
+			result: function (response, error, http) {
+					if (!error) {
+						angular.forEach(response.Result.Course,
+							function(value){
+								var course = {};
+								course.id = value.CourseId;
+								course.name = value.Subject;
+								$scope.courses.push(course);
+							});
+						$scope.$apply(); //trigger view update
+					}
+				}
+			});
+	}
+}
